a4f665963fcfa7da0da0dc8d005db43978a9ba3c
RamonDiazRodrigo <Ramon.Diaz2@alu.uclm.es>
2020-11-02 19:21:42 +0100

    ver reunion primera version

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { LoginComponent } from './login/login.component';
-
-
 import { ReunionesComponent } from './reuniones/reuniones.component';
 import { ReunionesPreComponent } from './reuniones-pre/reuniones-pre.component';
-
+import { MatDialogModule } from '@angular/material/dialog'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MenuComponent } from './menu/menu.component';
 import { RegistroComponent } from './registro/registro.component';
@@ -33,7 +30,12 @@ import { ReunionComponent } from './reunion/reunion.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatDialogModule,
+    BrowserAnimationsModule
+  ],
+  entryComponents:[
+    ReunionComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
