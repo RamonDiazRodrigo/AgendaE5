577fdf77ef6982262e7d70cd8593cb3be22afb2e
chemabr24 <JoseManuel.Bastante@alu.uclm.es>
2020-11-05 12:01:42 +0100

    Metodo encriptar

---
+++ src/main/java/com/ae5/sige/encryption/Encriptacion.java
@@ -0,0 +1,44 @@
+package com.ae5.sige.encryption;
+
+import java.security.MessageDigest;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class Encriptacion {
+
+	/**
+	   * Método para encriptar texto.
+	   * 
+	   * @author e3corp
+	   */
+	  public static String encriptar(final String texto) {
+
+	    final String secretKey = "ae5sige"; // llave para encriptar datos
+	    String base64EncryptedString = "";
+
+	    try {
+
+	      final MessageDigest md = MessageDigest.getInstance("MD5");
+	      final byte[] digestOfPassword = md.digest(secretKey.getBytes("utf-8"));
+	      final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+
+	      final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+	      final Cipher cipher = Cipher.getInstance("DESede");
+	      cipher.init(Cipher.ENCRYPT_MODE, key);
+
+	      final byte[] plainTextBytes = texto.getBytes("utf-8");
+	      final byte[] buf = cipher.doFinal(plainTextBytes);
+	      final byte[] base64Bytes = Base64.encodeBase64(buf);
+	      base64EncryptedString = new String(base64Bytes);
+
+	    } catch (Exception ex) {
+	    }
+	    return base64EncryptedString;
+	  }
+
+}
