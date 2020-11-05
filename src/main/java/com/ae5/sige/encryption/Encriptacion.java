f8865bf3d75ad4e080fdad75a6217526d6af1392
chemabr24 <JoseManuel.Bastante@alu.uclm.es>
2020-11-05 12:40:09 +0100

    Metodos desencriptar listas de usuarios

--- src/main/java/com/ae5/sige/encryption/Encriptacion.java
+++ src/main/java/com/ae5/sige/encryption/Encriptacion.java
@@ -1,7 +1,9 @@
 package com.ae5.sige.encryption;
 
 import java.security.MessageDigest;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import javax.crypto.Cipher;
@@ -15,109 +17,145 @@ import com.ae5.sige.model.Usuario;
 public class Encriptacion {
 
 	/**
-	   * Método para encriptar texto.
-	   * 
-	   * @author e3corp
-	   */
-	  public static String encriptar(final String texto) {
+	 * Método para encriptar texto.
+	 * 
+	 * @author e3corp
+	 */
+	public static String encriptar(final String texto) {
 
-	    final String secretKey = "ae5sige"; // llave para encriptar datos
-	    String base64EncryptedString = "";
+		final String secretKey = "ae5sige"; // llave para encriptar datos
+		String base64EncryptedString = "";
 
-	    try {
+		try {
 
-	      final MessageDigest md = MessageDigest.getInstance("MD5");
-	      final byte[] digestOfPassword = md.digest(secretKey.getBytes("utf-8"));
-	      final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+			final MessageDigest md = MessageDigest.getInstance("MD5");
+			final byte[] digestOfPassword = md.digest(secretKey.getBytes("utf-8"));
+			final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
 
-	      final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
-	      final Cipher cipher = Cipher.getInstance("DESede");
-	      cipher.init(Cipher.ENCRYPT_MODE, key);
+			final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+			final Cipher cipher = Cipher.getInstance("DESede");
+			cipher.init(Cipher.ENCRYPT_MODE, key);
 
-	      final byte[] plainTextBytes = texto.getBytes("utf-8");
-	      final byte[] buf = cipher.doFinal(plainTextBytes);
-	      final byte[] base64Bytes = Base64.encodeBase64(buf);
-	      base64EncryptedString = new String(base64Bytes);
+			final byte[] plainTextBytes = texto.getBytes("utf-8");
+			final byte[] buf = cipher.doFinal(plainTextBytes);
+			final byte[] base64Bytes = Base64.encodeBase64(buf);
+			base64EncryptedString = new String(base64Bytes);
 
-	    } catch (Exception ex) {
-	    }
-	    return base64EncryptedString;
-	  }
-	  
-	  /**
-	   * Método para desencriptar texto.
-	   * 
-	   * @author ae5
-	   */
+		} catch (Exception ex) {
+		}
+		return base64EncryptedString;
+	}
 
-	  public static String desencriptar(final String textoEncriptado) throws Exception {
+	/**
+	 * Método para desencriptar texto.
+	 * 
+	 * @author ae5
+	 */
 
-	    final String secretKey = "ae5sige"; // llave para desencriptar datos
-	    String base64EncryptedString = "";
+	public static String desencriptar(final String textoEncriptado) throws Exception {
 
-	    try {
-	      final byte[] message = Base64.decodeBase64(textoEncriptado.getBytes("utf-8"));
-	      final MessageDigest md = MessageDigest.getInstance("MD5");
-	      final byte[] digestOfPassword = md.digest(secretKey.getBytes("utf-8"));
-	      final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
-	      final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+		final String secretKey = "ae5sige"; // llave para desencriptar datos
+		String base64EncryptedString = "";
 
-	      final Cipher decipher = Cipher.getInstance("DESede");
-	      decipher.init(Cipher.DECRYPT_MODE, key);
+		try {
+			final byte[] message = Base64.decodeBase64(textoEncriptado.getBytes("utf-8"));
+			final MessageDigest md = MessageDigest.getInstance("MD5");
+			final byte[] digestOfPassword = md.digest(secretKey.getBytes("utf-8"));
+			final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+			final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
 
-	      final byte[] plainText = decipher.doFinal(message);
+			final Cipher decipher = Cipher.getInstance("DESede");
+			decipher.init(Cipher.DECRYPT_MODE, key);
 
-	      base64EncryptedString = new String(plainText, "UTF-8");
+			final byte[] plainText = decipher.doFinal(message);
 
-	    } catch (Exception ex) {
-	    }
-	    return base64EncryptedString;
-	  }
-	  
-	  /**
-	   * Método para desencriptar un  optional usuario.
-	   * 
-	   * @author ae5
-	   */
-	  public static Optional<Usuario> desencriptarOptionalUsuario(final Optional<Usuario> user) {
+			base64EncryptedString = new String(plainText, "UTF-8");
 
-			try {
-				user.get().setContrasena(desencriptar(user.get().getContrasena()));
-				user.get().setNombre(desencriptar(user.get().getNombre()));
-				user.get().setApellidos(desencriptar(user.get().getApellidos()));
-				user.get().setDni(desencriptar(user.get().getDni()));
-				user.get().setTelefono(desencriptar(user.get().getTelefono()));
-				user.get().setCorreo(desencriptar(user.get().getCorreo()));
+		} catch (Exception ex) {
+		}
+		return base64EncryptedString;
+	}
 
-				return user;
-			} catch (Exception ex) {
+	/**
+	 * Método para desencriptar un optional usuario.
+	 * 
+	 * @author ae5
+	 */
+	public static Optional<Usuario> desencriptarOptionalUsuario(final Optional<Usuario> usuario) {
 
-				return null;
-			}
+		try {
+			usuario.get().setContrasena(desencriptar(usuario.get().getContrasena()));
+			usuario.get().setNombre(desencriptar(usuario.get().getNombre()));
+			usuario.get().setApellidos(desencriptar(usuario.get().getApellidos()));
+			usuario.get().setDni(desencriptar(usuario.get().getDni()));
+			usuario.get().setTelefono(desencriptar(usuario.get().getTelefono()));
+			usuario.get().setCorreo(desencriptar(usuario.get().getCorreo()));
+
+			return usuario;
+		} catch (Exception ex) {
+
+			return null;
+		}
+
+	}
+
+	/**
+	 * Método para desencriptar usuario.
+	 * 
+	 * @author ae5
+	 */
+	public static Usuario desencriptarUsuario(final Usuario usuario) {
+
+		try {
+
+			usuario.setContrasena(desencriptar(usuario.getContrasena()));
+			usuario.setNombre(desencriptar(usuario.getNombre()));
+			usuario.setApellidos(desencriptar(usuario.getApellidos()));
+			usuario.setDni(desencriptar(usuario.getDni()));
+			usuario.setTelefono(desencriptar(usuario.getTelefono()));
+			usuario.setCorreo(desencriptar(usuario.getCorreo()));
+			return usuario;
+		} catch (Exception ex) {
+
+			return null;
+		}
+
+	}
+
+	/**
+	 * Método para desencriptar una optional lista de usuarios.
+	 * 
+	 * @author ae5
+	 */
+	public static List<Usuario> desencriptarListaUsuarios(final Optional<List<Usuario>> usuarios) {
+
+		final List<Usuario> usuariosDesencriptados = new ArrayList<Usuario>();
+		for (int i = 0; i < usuarios.get().size(); i++) {
+			final Usuario usuario = usuarios.get().get(i);
+			usuariosDesencriptados.add(desencriptarUsuario(usuario));
 
 		}
 
-		  /**
-		   * Método para desencriptar usuario.
-		   * 
-		   * @author ae5
-		   */
-		  public static Usuario desencriptarUsuario(final Usuario user) {
+		return usuariosDesencriptados;
+	}
 
-		    try {
+	/**
+	 * Método para desencriptar una lista de usuarios.
+	 * 
+	 * @author ae5
+	 */
+	public static List<Usuario> desencriptarUsuarios(final List<Usuario> usuarios) {
 
-		    	user.setContrasena(desencriptar(user.getContrasena()));
-				user.setNombre(desencriptar(user.getNombre()));
-				user.setApellidos(desencriptar(user.getApellidos()));
-				user.setDni(desencriptar(user.getDni()));
-				user.setTelefono(desencriptar(user.getTelefono()));
-				user.setCorreo(desencriptar(user.getCorreo()));
-		      return user;
-		    } catch (Exception ex) {
+		final List<Usuario> usuariosDesencriptados = new ArrayList<Usuario>();
 
-		      return null;
-		    }
+		for (int i = 0; i < usuarios.size(); i++) {
+			final Usuario usuario = usuarios.get(i);
 
-		  }
+			usuariosDesencriptados.add(desencriptarUsuario(usuario));
+
+		}
+
+		return usuariosDesencriptados;
+	}
 
 }
